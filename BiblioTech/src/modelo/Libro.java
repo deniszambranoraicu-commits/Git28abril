d49ad68c497d8e25205c3ee878d2cfa137150de4
Denis Brayan <deniszambranoraicu@gmail.com>
2026-04-28 09:46:58 +0200

    Paquetes y atributos encapsulados

--- BiblioTech/src/modelo/Libro.java
+++ BiblioTech/src/modelo/Libro.java
@@ -1,4 +1,4 @@
-package com.bibliotech;
+package modelo;
 
 import java.io.Serializable;
 
@@ -9,10 +9,10 @@ import java.io.Serializable;
  */
 public class Libro implements Serializable {
 
-	public String isbn;
-	public String titulo;
-	public String autor;
-	public boolean prestado;
+	String isbn;
+	private String titulo;
+	private String autor;
+	private boolean prestado;
 
 	public Libro(String isbn, String titulo, String autor) {
 		this.isbn = isbn;
@@ -32,5 +32,37 @@ public class Libro implements Serializable {
 	@Override
 	public String toString() {
 		return String.format("%-13s | %-30s | %-20s | %s", isbn, titulo, autor, prestado ? "Prestado" : "Disponible");
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public boolean isPrestado() {
+		return prestado;
+	}
+
+	public void setPrestado(boolean prestado) {
+		this.prestado = prestado;
 	}
 }
